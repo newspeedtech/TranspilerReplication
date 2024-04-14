4aaaa8b70c8e4d4065d837b2f675b2dee707fe8e
newspeed <mack76801@yahoo.com>
2024-04-14 13:44:11 -0500

    adding project

---
+++ src/main/kotlin/MyKotlinFile.kt
@@ -0,0 +1,22 @@
+import kotlin.text.*
+
+fun main() {
+    printHelloWorld()
+    printing(arrayOf("Marla"))
+
+    // This is an end-of-line comment
+
+    /* This is a block comment
+   on multiple lines. */
+}
+
+fun printHelloWorld(){
+    print("Hello World")
+}
+
+fun printing(args: Array<String>) {
+    println("What is your name?")
+}
+
+
+
---
+++ src/test/kotlin/com/transpiler/SequalskTest.kt
@@ -0,0 +1,9 @@
+package com.transpiler
+
+class SequalskTest {
+    @org.junit.jupiter.api.Test
+    internal fun testPrinting() {
+        assert(true)
+        TODO("Not yet implemented")
+    }
+}
