e9f033ead547fea5b999e9074cd03cbd17cfe013
newspeedtech <mack76801@yahoo.com>
2024-04-19 11:32:24 -0500

    adding statements

--- src/main/kotlin/MyKotlinFile.kt
+++ src/main/kotlin/MyKotlinFile.kt
@@ -40,3 +40,75 @@ fun arbitraryExpInTemplate(): String {
 
 
 fun conditionalAsExpression(a: Int, b: Int) = if (a > b) a else b
+
+fun describe(obj: Any): String =
+    when (obj) {
+        1 -> "One"
+        "Hello" -> "Greeting"
+        is Long -> "Long"
+        !is String -> "Not a string"
+        else -> "Unknown"
+    }
+
+fun withinRange(x: Int, y: Int): Boolean {
+    return x in 1..y + 1
+}
+
+fun inCollection(items: Array<String>): String {
+    var str = "Nothing to say."
+    when {
+        "orange" in items -> str = "juicy"
+        "apple" in items -> str = "apple is fine too"
+    }
+    return str
+}
+
+fun collectionLambdaExpression(list: Array<String>): MutableList<String> {
+    val filteredList = mutableListOf<String>()
+    list
+        .filter { it.startsWith("a") }
+        .sortedBy { it }
+        .map { it.toUpperCase() }
+        .forEach { filteredList.add(it) }
+
+    return filteredList
+}
+
+fun parseInt(str: String): Int? {
+    return str.toIntOrNull()
+}
+fun getProduct(arg1: String, arg2: String): Int? {
+    val x = parseInt(arg1)
+    val y = parseInt(arg2)
+    if (x != null && y != null) {
+        return (x * y)
+    }
+    else {
+        println("'$arg1' or '$arg2' is not a number")
+        return null
+    }
+}
+fun castNumber(arg1: String, arg2: String): Int? {
+    val x = parseInt(arg1)
+    val y = parseInt(arg2)
+    if (x == null) {
+        println("Wrong number format in arg1: '$arg1'")
+        return null
+    }
+    if (y == null) {
+        println("Wrong number format in arg2: '$arg2'")
+        return null
+    }
+    return (x * y)
+}
+
+fun getProductFromOptional(x: Int?, y: Int?): Int? {
+    if (x != null && y != null) {
+        return (x * y)
+    }
+    else {
+        println("'$x' or '$y' is not a number.")
+        return null
+    }
+}
+
