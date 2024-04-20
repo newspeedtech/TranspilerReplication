17d9aa045b0585b37f7a50a37a2ce3b3e94499d8
newspeedtech <mack76801@yahoo.com>
2024-04-20 09:07:49 -0500

    adding statements

--- src/main/kotlin/MyKotlinFile.kt
+++ src/main/kotlin/MyKotlinFile.kt
@@ -112,3 +112,26 @@ fun getProductFromOptional(x: Int?, y: Int?): Int? {
     }
 }
 
+fun getStringLength1(obj: Any): Int? {
+    if (obj is String) {
+        return obj.length
+    }
+    return null
+}
+fun getStringLength2(obj: Any): Int? {
+    if (obj !is String) return null
+    return obj.length
+}
+fun getStringLength3(obj: Any): Int? {
+    if (obj is String && obj.length > 0) {
+        return obj.length
+    }
+    return null
+}
+fun automaticCastOneVariable(arg1: Int?): Int? {
+    if (arg1 != null) {
+        return arg1 * 2
+    } else {
+        return null
+    }
+}
